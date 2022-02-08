00ed70500859f4e042f1718bb396f384d55bd5ef
bagusip <bagusindrt@gmail.com>
2022-02-08 12:37:43 +0700

    initial commit, with nav drawer

---
+++ app/src/main/java/id/bagusip/inixindoprojectindividu/MainActivity.java
@@ -0,0 +1,99 @@
+package id.bagusip.inixindoprojectindividu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+import id.bagusip.inixindoprojectindividu.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private ActionBarDrawerToggle toggle;
+    Toolbar toolbar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        // custom toolbar
+        setSupportActionBar(binding.toolbar);
+
+        //default fragment yang dibuka pertama kali. (menu home muncul pertama kali)
+        getSupportActionBar().setTitle("Home");
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_layout, new HomeFragment())
+                .commit();
+        binding.navView.setCheckedItem(R.id.nav_home);
+
+        //membuka drawer
+        toggle = new ActionBarDrawerToggle(this, binding.drawer, binding.toolbar, R.string.open, R.string.close);
+
+        //drawer back button
+        toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.white));
+
+        // sinkronisasi drawer
+        toggle.syncState();
+
+        //salah satu menu navigasi dipilih
+        binding.navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            Fragment fragment = null;
+
+            //option selected draw
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        fragment = new HomeFragment();
+                        getSupportActionBar().setTitle("Home");
+                        binding.drawer.closeDrawer(GravityCompat.START);
+                        callFragment(fragment);
+                        break;
+                }
+                return true;
+            }
+        });
+
+        NavigationView navigationView = findViewById(R.id.navView);
+        View headerView = getLayoutInflater().inflate(R.layout.nav_header_layout, navigationView, false);
+        navigationView.addHeaderView(headerView);
+
+    }
+
+    private void callFragment(Fragment fragment) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.setCustomAnimations(
+                android.R.anim.slide_in_left,
+                android.R.anim.slide_out_right
+        );
+        transaction.replace(R.id.frame_layout, fragment);
+        //menunya bergantian
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+}
